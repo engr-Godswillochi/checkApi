9a90749ae4b20c1a30d38b212358babae1acd9b0
engr-Godswillochi <ochigodswill@gmail.com>
2024-11-01 09:13:51 +0100

    inital commit

---
+++ TestApi/Models/User.cs
@@ -0,0 +1,75 @@
+﻿using TestApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.AspNetCore.Http.HttpResults;
+namespace TestApi.Models
+{
+    public class User
+    {
+        required public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+
+    }
+
+
+public static class UserEndpoints
+{
+	public static void MapUserEndpoints (this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/api/User").WithTags(nameof(User));
+
+        group.MapGet("/", async (UserContext db) =>
+        {
+            return await db.Users.ToListAsync();
+        })
+        .WithName("GetAllUsers")
+        .WithOpenApi();
+
+        group.MapGet("/{id}", async Task<Results<Ok<User>, NotFound>> (int id, UserContext db) =>
+        {
+            return await db.Users.AsNoTracking()
+                .FirstOrDefaultAsync(model => model.Id == id)
+                is User model
+                    ? TypedResults.Ok(model)
+                    : TypedResults.NotFound();
+        })
+        .WithName("GetUserById")
+        .WithOpenApi();
+
+        group.MapPut("/{id}", async Task<Results<Ok, NotFound>> (int id, User user, UserContext db) =>
+        {
+            var affected = await db.Users
+                .Where(model => model.Id == id)
+                .ExecuteUpdateAsync(setters => setters
+                  .SetProperty(m => m.Id, user.Id)
+                  .SetProperty(m => m.Name, user.Name)
+                  .SetProperty(m => m.Email, user.Email)
+                  .SetProperty(m => m.Password, user.Password)
+                  );
+            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+        })
+        .WithName("UpdateUser")
+        .WithOpenApi();
+
+        group.MapPost("/", async (User user, UserContext db) =>
+        {
+            db.Users.Add(user);
+            await db.SaveChangesAsync();
+            return TypedResults.Created($"/api/User/{user.Id}",user);
+        })
+        .WithName("CreateUser")
+        .WithOpenApi();
+
+        group.MapDelete("/{id}", async Task<Results<Ok, NotFound>> (int id, UserContext db) =>
+        {
+            var affected = await db.Users
+                .Where(model => model.Id == id)
+                .ExecuteDeleteAsync();
+            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+        })
+        .WithName("DeleteUser")
+        .WithOpenApi();
+    }
+}}
